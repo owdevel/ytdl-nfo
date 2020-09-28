a685a7533c632de35d4ef0588ffd097ab3107f78
Owen Mak <owdevel@gmail.com>
2020-09-29 02:02:23 +1000

    Changed to use _filename tag and remove .info

--- ytdl_nfo/Ytdl_nfo.py
+++ ytdl_nfo/Ytdl_nfo.py
@@ -5,18 +5,19 @@ import nfo
 class Ytdl_nfo:
     def __init__(self, file_path):
         self.path = file_path
-        self.filename = os.path.splitext(file_path)[0]
+        self.dir = os.path.dirname(file_path)
 
         # Read json data
         with open(self.path, 'r') as f:
             self.data = json.load(f)
         self.extractor = self.data['extractor'].lower()
+        self.filename = os.path.splitext(self.data['_filename'])[0]
         self.nfo = nfo.get_config(self.extractor)
 
     def process(self):
         self.nfo.generate(self.data)
         self.nfo.print_nfo()
-        self.nfo.write_nfo(f'{self.filename}.nfo')
+        self.nfo.write_nfo(os.path.join(self.dir, f'{self.filename}.nfo'))
 
     def print_data(self):
         print(json.dumps(self.data, indent=4, sort_keys=True))
