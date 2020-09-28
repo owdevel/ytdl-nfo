69dc2b8ea23aff3c44f1ab164793bc21cc4b5273
Owen Mak <owdevel@gmail.com>
2020-09-29 01:39:11 +1000

    Added directory parse support

--- ytdl_nfo/Ytdl_nfo.py
+++ ytdl_nfo/Ytdl_nfo.py
@@ -1,15 +1,16 @@
+import os
 import json
 import nfo
 
 class Ytdl_nfo:
     def __init__(self, file_path):
         self.path = file_path
+        self.filename = os.path.splitext(file_path)[0]
 
         # Read json data
         with open(self.path, 'r') as f:
             self.data = json.load(f)
         self.extractor = self.data['extractor'].lower()
-        self.filename = self.data['_filename'].split('.')[-2]
         self.nfo = nfo.get_config(self.extractor)
 
     def process(self):
--- ytdl_nfo/__main__.py
+++ ytdl_nfo/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 from Ytdl_nfo import Ytdl_nfo
 
 def main():
@@ -9,8 +10,14 @@ def main():
     parser.add_argument('input', metavar='JSON_FILE', type=str, help='Json file path')
     args = parser.parse_args()
 
-    file = Ytdl_nfo(args.input)
-    file.process()
+    if os.path.isfile(args.input):
+        file = Ytdl_nfo(args.input)
+        file.process()
+    else:
+        for filename in os.listdir(args.input):
+            if filename.endswith('.json'):
+                file = Ytdl_nfo(os.path.join(args.input,  filename))
+                file.process()
 
 
 if __name__=='__main__':
