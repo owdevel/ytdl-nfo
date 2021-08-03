ffd73700fb834218358432291314e916f01d20d6
Sophie Kirschner <pineapplemachine@gmail.com>
2021-08-03 15:30:24 +0300

    Recognize "--overwrite" flag in CLI args

--- ytdl_nfo/__init__.py
+++ ytdl_nfo/__init__.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description='ytdl_nfo, a youtube-dl helper to convert the output of  \'youtube-dl --write-info-json\' to an NFO for use in kodi/plex/etc')
     parser.add_argument('input', metavar='JSON_FILE', type=str, help='Json file to convert or folder to convert in')
     parser.add_argument('-e', '--extractor', help='Specify specific extractor')
+    parser.add_argument('-w', '--overwrite', action="store_true", help='Overwrite existing NFO files')
     parser.add_argument('--config', help='Prints the path to the config directory', action='version', version=f'{get_config_path()}')
     args = parser.parse_args()
 
@@ -19,9 +20,10 @@ def main():
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 if file_name.endswith('.info.json'):
-                    print(f'Processing {file_path} with extractor {args.extractor}')
-                    file = Ytdl_nfo(file_path, args.extractor)
-                    file.process()
+                    if args.overwrite or not os.path.exists(file_path[:-10] + ".nfo"):
+                        print(f'Processing {file_path} with extractor {args.extractor}')
+                        file = Ytdl_nfo(file_path, args.extractor)
+                        file.process()
 
 def get_config_path():
     return os.path.join(os.path.dirname(__file__), 'configs')
